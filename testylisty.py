4d89e521ed35e3f59a63aae561369cab8c97f790
Optionalnie <mikolaj.matenka@gmail.com>
2024-01-09 12:00:54 +0100

    Commit po raz pierwszy

--- testylisty.py
+++ testylisty.py
@@ -1,22 +1,24 @@
 import sys
 
+shop_list = []
+
 while True:
-    shop_list = []
+
     decision = input('No elo, robimy zakupową listę? Wpisz "zakupy" aby zacząć tworzyć listę albo "exit" aby wyjsc z apki ew. wpisz "lista" aby wyswietlic aktualna liste zakupów: ')
 
     if decision == 'zakupy':
         while True:
             zakupy = input('Wpisz listę zakupów - Napisz "stop" aby zakończyć dodaniwe zakupów ')
-
             if zakupy.lower() == 'stop':
                 break
+            shop_list.append(zakupy)
         
     elif decision == 'exit':
         sys.exit
         
     elif decision == 'lista':
         print('Oto Twoja lista zakupów: ' + str(shop_list))    
-        shop_list.append(zakupy)
+        
         
     else:
         print('Zła komenda')
